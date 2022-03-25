edb8839fff0db70899d259c81dfa8edf9b5ad529
Alessa Santos <alessa.bcc@gmail.com>
2022-03-25 16:51:41 -0300

    Finalização do projeto de Restaurante com React e typescript

---
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,35 @@
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+import { constants } from 'buffer';
+
+interface Props{
+    ordenador: string,
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({
+    ordenador,
+    setOrdenador
+}: Props){
+    const [aberto,sertAberto] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+
+    return(
+        <button className={classNames(
+            {[styles.ordenador]: true,
+             [styles["ordenador--ativo"]] : ordenador !== "",
+            
+            })}onClick={()=>sertAberto(!aberto)} onBlur={() => sertAberto(false)}>
+            <span>{nomeOrdenador || "Ordenar Por"}</span>
+            {aberto ? < MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+            <div className={classNames({[styles.ordenador__options]: true, [styles["ordenador__options--ativo"]]: aberto })}>{opcoes.map(opcao => (
+                <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
+                    {opcao.nome}
+                </div>
+            ))}</div>
+        </button>
+    )
+}
